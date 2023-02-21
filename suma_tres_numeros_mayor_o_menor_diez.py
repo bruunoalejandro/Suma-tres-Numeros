f6e5cd3b3cb0055672934bc5ba1cd3d7e03693d1
Bruno Fernandes <125995286+bruunoalejandro@users.noreply.github.com>
2023-02-21 15:03:09 -0300

    Add files via upload

---
+++ suma_tres_numeros_mayor_o_menor_diez.py
@@ -0,0 +1,8 @@
+x = int(input("Ingrese el primer numero: "))
+y = int(input("Ingrese el segundo numero: "))
+z = int(input("Ingrese el tercer numero: "))
+res = x + y + z
+if res > 10:
+    print("El resultado es", res,"y es mayor que 10")
+else:
+    print("El resultado es", res,"y es menor que 10")
